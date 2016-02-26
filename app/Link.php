8b859b21a9e951a3112c0bd57cfc337cc77fed36
ses10 <sesma@live.com>
2016-02-26 09:14:50 -0500

    add findUrl()

--- app/Link.php
+++ app/Link.php
@@ -2,9 +2,16 @@
 
 namespace App;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model
-{
-    //
+{	
+	protected $primaryKey = 'linkId';
+
+	//queries the database for a url and returns it
+	static public function findUrl($linkId)
+	{
+		return Link::find($linkId)->url;
+	}
 }
