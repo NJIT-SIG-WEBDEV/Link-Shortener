62110e89e08b2cf03c1e101e25d86a4d21556333
ses10 <sesma@live.com>
2016-02-26 09:16:18 -0500

    finish redirect()

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -4,18 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Link;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class RedirectController extends Controller
 {
-    //
+
     public function redirect($linkId)
     {
-    	return 'The link ID you entered was: ' . $linkId;
-
-    	//pull id from database
-
-    	
+    	//pull url from database
+    	$url = Link::findUrl($linkId);
+    	return redirect($url);
     }
 }
