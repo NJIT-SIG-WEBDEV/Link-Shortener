f5b641d756aacd483fcf753183b60fe8f634d1b7
ses10 <sesma@live.com>
2016-03-23 17:47:01 -0400

    findUrl() now uses code field to find

--- app/Link.php
+++ app/Link.php
@@ -7,11 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model
 {	
-	protected $primaryKey = 'linkId';
 
 	//queries the database for a url and returns it
-	static public function findUrl($linkId)
+	static public function findUrl($linkCode)
 	{
-		return Link::find($linkId)->url;
+		return Link::where('code', $linkCode)->first()->url;
 	}
 }
