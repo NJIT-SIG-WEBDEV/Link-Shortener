701ee7e3161bde4369e67cbdb43c11195a0e4c19
ses10 <sesma@live.com>
2016-03-23 18:33:05 -0400

    add form create shortened link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,19 @@
 	Laravel Link Shortener | SIG Web Dev
 @stop
 
-@section('content')
+@section('content') 
 	Home
 	<hr>
 
+
+	<h3>Shorten A URL!</h3>
+	<form method="POST" action="/">
+		<div class="form-group">
+			<input class="form-control" type="text" name="url" placeholder="Enter URL"></input>
+		</div>
+		<div class="form-group">
+			<button type="submit" class="btn btn-primary">Submit</button>
+		</div>		
+	</form>
 
 @stop
